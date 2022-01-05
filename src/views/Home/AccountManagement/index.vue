0d594ba2891c2b53e2ec343d7dc9e6734053b0e9
熊熊熊子路 <1711279251@qq.com>
2022-01-06 02:20:38 +0800

    feat：实现删除账户

--- src/views/Home/AccountManagement/index.vue
+++ src/views/Home/AccountManagement/index.vue
@@ -12,23 +12,25 @@
       <el-button type="primary" @click="search">搜索</el-button>
       <el-button type="primary" @click="clearSearch">清除搜索结果</el-button>
     </el-card>
-<!--    添加-->
-      <el-card shadow="always" class="add-account">
-          <h1 class="tip-title">添加账户</h1>
-          <el-input v-model="addAccount.accountInput" class="account-input" placeholder="请输入账号"></el-input>
-          <el-input type="password" v-model="addAccount.passwordInput" class="password-input" placeholder="请输入密码"></el-input>
-          <el-input type="password" v-model="addAccount.checkPasswordInput" class="check-password-input" placeholder="请再次输入密码"></el-input>
-          <el-select class="account-type-select" v-model="addAccount.roleType" placeholder="选择职业">
-            <el-option value="超级管理员">超级管理员</el-option>
-            <el-option value="商品管理员">商品管理员</el-option>
-            <el-option value="客服专员">客服专员</el-option>
-          </el-select>
-          <el-button type="primary" @click="handleAddAccount">添加</el-button>
-          <el-button type="danger">清空</el-button>
-      </el-card>
+    <!--    添加-->
+    <el-card shadow="always" class="add-account">
+      <h1 class="tip-title">添加账户</h1>
+      <el-input v-model="addAccount.accountInput" class="account-input" placeholder="请输入账号"></el-input>
+      <el-input type="password" v-model="addAccount.passwordInput" class="password-input"
+                placeholder="请输入密码"></el-input>
+      <el-input type="password" v-model="addAccount.checkPasswordInput" class="check-password-input"
+                placeholder="请再次输入密码"></el-input>
+      <el-select class="account-type-select" v-model="addAccount.roleType" placeholder="选择职业">
+        <el-option value="超级管理员">超级管理员</el-option>
+        <el-option value="商品管理员">商品管理员</el-option>
+        <el-option value="客服专员">客服专员</el-option>
+      </el-select>
+      <el-button type="primary" @click="handleAddAccount">添加</el-button>
+      <el-button type="danger">清空</el-button>
+    </el-card>
     <!--  表格-->
     <el-table
-        v-loading = "loading"
+        v-loading="loading"
         :data="accountDataShow"
         border
         height="335"
@@ -81,11 +83,13 @@
     >
       <div class="account-edit-drawer__content">
         <el-form :model="editAccount" label-width="60">
-          <el-form-item label="账号" >
-            <el-input class="drawer-input" v-model="editAccount.account" placeholder="请输入账号" autocomplete="off"></el-input>
+          <el-form-item label="账号">
+            <el-input class="drawer-input" v-model="editAccount.account" placeholder="请输入账号"
+                      autocomplete="off"></el-input>
           </el-form-item>
-          <el-form-item label="密码" >
-            <el-input class="drawer-input" type="password" show-password v-model="editAccount.password" placeholder="请输入密码" autocomplete="off"></el-input>
+          <el-form-item label="密码">
+            <el-input class="drawer-input" type="password" show-password v-model="editAccount.password"
+                      placeholder="请输入密码" autocomplete="off"></el-input>
           </el-form-item>
           <el-form-item label="职位">
             <el-select v-model="editAccount.roleType" placeholder="请选择职位">
@@ -106,45 +110,46 @@
 
 <script>
 import checkInput from '../../../utils/checkInput'
+
 export default {
   name: "AccountManagement",
   data() {
     return {
       // 添加账户的数据
-      addAccount:{
-        accountInput:'',
-        passwordInput:'',
-        checkPasswordInput:'',
-        roleType:''
+      addAccount: {
+        accountInput: '',
+        passwordInput: '',
+        checkPasswordInput: '',
+        roleType: ''
       },
       // 编辑账户
-      editAccount:{
-        id:'',
-        account:'',
-        password:'',
-        roleType:''
+      editAccount: {
+        id: '',
+        account: '',
+        password: '',
+        roleType: ''
       },
       // 搜索模块的数据
-      searchAccount:{
-        searchInput:'',
-        searchTypeShow:'',
-        searchTypeData:''
+      searchAccount: {
+        searchInput: '',
+        searchTypeShow: '',
+        searchTypeData: ''
       },
       // 后端返回的数据
       accountData: [],
-      accountDataShow:[],
+      accountDataShow: [],
       // 控制 loading 遮罩
       loading: false,
       // 控制 抽屉 开关
-      isDrawerShow:false
+      isDrawerShow: false
     }
   },
-  methods:{
+  methods: {
     /**
      * 查询全部账户信息的函数
      */
-    async getAllAccount(){
-      try{
+    async getAllAccount() {
+      try {
         // 开启 loading 遮罩
         this.loading = true;
         // 发送请求
@@ -156,7 +161,7 @@ export default {
         this.accountData = this.accountDataShow = newRes;
         // 关闭 loading 遮罩
         this.loading = false;
-      }catch (err){
+      } catch (err) {
         console.log(err);
       }
     },
@@ -164,19 +169,19 @@ export default {
     /**
      * 根据传入的数据查询账户信息的函数
      */
-    search(){
+    search() {
       // 判断
-      if(!this.searchAccount.searchTypeData || !this.searchAccount.searchTypeShow) {
+      if (!this.searchAccount.searchTypeData || !this.searchAccount.searchTypeShow) {
         this.$message({
-          type:"warning",
-          message:"请选择类型"
+          type: "warning",
+          message: "请选择类型"
         })
         return;
       }
-      if(!this.searchAccount.searchInput) {
+      if (!this.searchAccount.searchInput) {
         this.$message({
-          type:"warning",
-          message:"请输入关键字"
+          type: "warning",
+          message: "请输入关键字"
         })
         return;
       }
@@ -194,66 +199,66 @@ export default {
     /**
      * 清除搜索结果
      */
-    clearSearch(){
-      this.searchAccount.searchTypeData ='';
-      this.searchAccount.searchTypeShow ='';
-      this.searchAccount.searchInput ='';
+    clearSearch() {
+      this.searchAccount.searchTypeData = '';
+      this.searchAccount.searchTypeShow = '';
+      this.searchAccount.searchInput = '';
       this.accountDataShow = this.accountData;
     },
 
     /**
      * 输入数据时进行验证工作
      */
-    addAccountCheck(){
-      if(!this.addAccount.accountInput) {
+    addAccountCheck() {
+      if (!this.addAccount.accountInput) {
         this.$message({
-          type:"warning",
-          message:"请填写账号"
+          type: "warning",
+          message: "请填写账号"
         })
         return false;
       }
-      if(this.addAccount.accountInput.length > 10 || this.addAccount.accountInput.length < 6){
+      if (this.addAccount.accountInput.length > 10 || this.addAccount.accountInput.length < 6) {
         this.$message({
-          type:"warning",
-          message:"账号的长度在6-10之间哦"
-        })
-        return false;
-      }
-
-      if(!this.addAccount.passwordInput) {
-        this.$message({
-          type:"warning",
-          message:"请填写密码"
-        })
-        return false;
-      }
-      if(this.addAccount.passwordInput.length < 6 || this.addAccount.passwordInput.length > 10){
-        this.$message({
-          type:"warning",
-          message:"密码的长度在6-10之间哦"
+          type: "warning",
+          message: "账号的长度在6-10之间哦"
         })
         return false;
       }
 
-      if(!this.addAccount.checkPasswordInput) {
+      if (!this.addAccount.passwordInput) {
         this.$message({
-          type:"warning",
-          message:"请再次填写密码"
+          type: "warning",
+          message: "请填写密码"
         })
         return false;
       }
-      if(this.addAccount.checkPasswordInput !== this.addAccount.passwordInput){
+      if (this.addAccount.passwordInput.length < 6 || this.addAccount.passwordInput.length > 10) {
         this.$message({
-          type:"warning",
-          message:"两次密码填写不一致，请重新填写"
+          type: "warning",
+          message: "密码的长度在6-10之间哦"
         })
         return false;
       }
 
-      if(!this.addAccount.roleType){
+      if (!this.addAccount.checkPasswordInput) {
         this.$message({
-          type:"warning",
-          message:"请选择职业类型"
+          type: "warning",
+          message: "请再次填写密码"
+        })
+        return false;
+      }
+      if (this.addAccount.checkPasswordInput !== this.addAccount.passwordInput) {
+        this.$message({
+          type: "warning",
+          message: "两次密码填写不一致，请重新填写"
+        })
+        return false;
+      }
+
+      if (!this.addAccount.roleType) {
+        this.$message({
+          type: "warning",
+          message: "请选择职业类型"
         })
         return false;
       }
@@ -268,22 +273,22 @@ export default {
       const res2 = checkInput.passwordCheck(this.addAccount.passwordInput, this);
       const res3 = checkInput.rePasswordCheck(this.addAccount.passwordInput, this.addAccount.checkPasswordInput, this);
       const res4 = checkInput.roleCheck(this.addAccount.roleType, this);
-      if (res1 && res2 && res3 && res4){
+      if (res1 && res2 && res3 && res4) {
         // 发起请求
-        const res = await this.$axios.post('/admin/account/add',{
-          account:this.addAccount.accountInput,
-          password:this.addAccount.passwordInput,
-          role:this.addAccount.roleType
+        const res = await this.$axios.post('/admin/account/add', {
+          account: this.addAccount.accountInput,
+          password: this.addAccount.passwordInput,
+          role: this.addAccount.roleType
         })
         this.$message({
           type: res.code === 200 ? "success" : "error",
-          message:res.msg
+          message: res.msg
         })
         // 清空输入
-        if (res.code === 200){
+        if (res.code === 200) {
           this.addAccount.accountInput = '';
           this.addAccount.passwordInput = '';
-          this.addAccount.checkPasswordInput ='';
+          this.addAccount.checkPasswordInput = '';
           this.addAccount.roleType = '';
 
           // 刷新表格
@@ -296,9 +301,9 @@ export default {
      * 开启编辑账户抽屉的函数
      * @param scoped
      */
-    openEditDrawer(scoped){
+    openEditDrawer(scoped) {
       // 配置数据
-      const {id, account, password, role } = scoped.row;
+      const {id, account, password, role} = scoped.row;
       this.editAccount = {
         id,
         account,
@@ -312,7 +317,7 @@ export default {
     /**
      * 在抽屉关闭之前提醒用户
      */
-    isGiveUpEdit(){
+    isGiveUpEdit() {
       // 开启弹框
       this.$confirm('您确定要退出吗?', '警告', {
         confirmButtonText: '确定',
@@ -326,48 +331,72 @@ export default {
     /**
      * 提交编辑完的账户信息到服务器
      */
-    async editAccountFunc(){
+    async editAccountFunc() {
       // 判断
       const res1 = checkInput.accountCheck(this.editAccount.account, this);
       const res2 = checkInput.passwordCheck(this.editAccount.password, this);
       const res3 = checkInput.roleCheck(this.editAccount.roleType, this);
 
-      if (res1 && res2 && res3){
+      if (res1 && res2 && res3) {
         // 提交信息
-        const res = await this.$axios.post('/admin/account/update',{
-          id:this.editAccount.id,
-          account:this.editAccount.account,
-          password:this.editAccount.password,
-          role:this.editAccount.roleType
+        const res = await this.$axios.post('/admin/account/update', {
+          id: this.editAccount.id,
+          account: this.editAccount.account,
+          password: this.editAccount.password,
+          role: this.editAccount.roleType
         })
         this.$message({
-          type: res.code === 200 ? "success":"error",
-          message:res.msg
+          type: res.code === 200 ? "success" : "error",
+          message: res.msg
         })
-        if(res.code === 200){
+        if (res.code === 200) {
           this.isDrawerShow = false;
           this.getAllAccount();
         }
       }
+    },
+
+    /**
+     * 删除一个账户
+     * @param id -> 需要删除的账户的id
+     * @returns {Promise<void>}
+     */
+    deleteAccount(id) {
+      // 弹出框框
+      this.$confirm('此操作将永久删除该账户, 是否继续?', '注意', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        // 提交信息
+        const res = await this.$axios.post('/admin/account/delete', {id});
+        this.$message({
+          type: res.code === 200 ? "success":"error",
+          message: res.msg
+        })
+        if (res.code === 200){
+          this.getAllAccount();
+        }
+      })
     }
   },
-  mounted(){
+  mounted() {
     // 向后端查询全部账户信息
     this.getAllAccount();
   },
-  watch:{
-    searchAccount:{
-      deep:true,
-      handler(newVal){
-        if (newVal.searchTypeShow === 'id'){
+  watch: {
+    searchAccount: {
+      deep: true,
+      handler(newVal) {
+        if (newVal.searchTypeShow === 'id') {
           newVal.searchTypeData = 'id';
           return;
         }
-        if (newVal.searchTypeShow === '账号'){
+        if (newVal.searchTypeShow === '账号') {
           newVal.searchTypeData = 'account';
           return;
         }
-        if (newVal.searchTypeShow === '职位'){
+        if (newVal.searchTypeShow === '职位') {
           newVal.searchTypeData = 'role';
         }
       }
@@ -377,46 +406,53 @@ export default {
 </script>
 
 <style scoped>
-.tip-title{
+.tip-title {
   font-size: 14px;
   color: #cecfd1;
   margin-bottom: 10px;
 }
-.search-type-select{
+
+.search-type-select {
   width: 10%;
   margin-right: 10px;
 }
+
 .search-input {
   width: 25%;
   margin-right: 10px;
 }
+
 .account-search-bar,
-.add-account{
+.add-account {
   margin-bottom: 20px;
 }
+
 .account-input,
 .password-input,
 .check-password-input,
-.account-type-select{
+.account-type-select {
   width: 20%;
   margin-right: 15px;
 }
 
 /* 抽屉 */
-.account-edit-drawer__content{
+.account-edit-drawer__content {
   height: 100%;
   position: relative;
-  padding: 0 20px ;
+  padding: 0 20px;
 }
-.drawer-input{
+
+.drawer-input {
   width: 90%;
 }
-.account-edit-drawer__footer{
+
+.account-edit-drawer__footer {
   position: absolute;
   bottom: 20px;
   right: 20px;
 }
-.drawer-button{
+
+.drawer-button {
   width: 100px;
 }
 </style>
