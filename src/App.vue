22c11b4c083c56e5238d4302da6dda1cfc6bf7d2
熊熊熊子路 <1711279251@qq.com>
2022-01-23 03:51:32 +0800

    代码格式化

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,46 @@
 <template>
-  <router-view></router-view>
+    <router-view></router-view>
 </template>
 
 <script>
 export default {
-  name:"APP",
-  created() {
-    //在页面加载时读取sessionStorage里的状态信息
-    if (sessionStorage.getItem("store")) {
-      this.$store.replaceState(
-          Object.assign(
-              {},
-              this.$store.state,
-              JSON.parse(sessionStorage.getItem("store"))
-          )
-      );
-    }
-    //在页面刷新时将vuex里的信息保存到sessionStorage里
-    window.addEventListener("beforeunload", () => {
-      sessionStorage.setItem("store", JSON.stringify(this.$store.state));
-    });
-  },
+    name: "APP",
+    created() {
+        //在页面加载时读取sessionStorage里的状态信息
+        if (sessionStorage.getItem("store")) {
+            this.$store.replaceState(
+                Object.assign(
+                    {},
+                    this.$store.state,
+                    JSON.parse(sessionStorage.getItem("store"))
+                )
+            );
+        }
+        //在页面刷新时将vuex里的信息保存到sessionStorage里
+        window.addEventListener("beforeunload", () => {
+            sessionStorage.setItem("store", JSON.stringify(this.$store.state));
+        });
+    },
 }
 </script>
 
 <style>
-*{
-  box-sizing: border-box;
+* {
+    box-sizing: border-box;
 }
-html,body{
-  height: 100%;
-  width: 100%;
-    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+
+html, body {
+    height: 100%;
+    width: 100%;
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
 }
-li{
-  list-style: none;
+
+li {
+    list-style: none;
 }
-a{
-  text-decoration: none;
-  color: white;
+
+a {
+    text-decoration: none;
+    color: white;
 }
 </style>
