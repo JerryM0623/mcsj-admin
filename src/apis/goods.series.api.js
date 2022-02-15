1534d67616db130821830ceca84e34b94a415b51
熊熊熊子路 <1711279251@qq.com>
2022-02-16 02:03:21 +0800

    feat: 添加获取全部系列的api定义

--- src/apis/goods.series.api.js
+++ src/apis/goods.series.api.js
@@ -1,5 +1,6 @@
 const seriesApis = {
     getSeriesByPageNum: '/admin/goods/series/getSeriesByPageNum',
+    getAllSeries: '/admin/goods/series/getAllSeries',
     addSeries: '/admin/goods/series/add',
     editSeries: '/admin/goods/series/edit',
     deleteSeries: '/admin/goods/series/delete'
