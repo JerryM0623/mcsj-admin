a44df1f732c26560905e63cb39310e4db814e8a9
熊熊熊子路 <1711279251@qq.com>
2022-02-16 00:34:31 +0800

    feat: 编写 types 的 api 列表

---
+++ src/apis/goods.types.api.js
@@ -0,0 +1,8 @@
+const typesApis = {
+    getTypesByPageNum: '/admin/goods/types/getByPage',
+    addType: '/admin/goods/types/addType',
+    editType: '/admin/goods/types/editType',
+    deleteType: '/admin/goods/types/deleteType'
+}
+
+export default typesApis;
