a0873617b40b3ffa4bdbf995cc56b0a7a8afee80
熊熊熊子路 <1711279251@qq.com>
2022-02-18 00:50:08 +0800

    feat: 实现窗户商品搜索与清除搜索痕迹

--- src/views/Home/Goods/Product/Window/index.vue
+++ src/views/Home/Goods/Product/Window/index.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="window">
         <el-card>
+            <!--功能区-->
+            <el-row style="margin-bottom: 10px">
+                <el-col :span="6">
+                    <el-input v-model="searchInput" style="width: 100%" placeholder="请输入关键词进行搜索"></el-input>
+                </el-col>
+                <el-col :span="18">
+                    <el-button @click="searchWindows" type="primary" style="margin-left: 10px">搜索</el-button>
+                    <el-button @click="clearSearch" type="primary">清除搜索信息</el-button>
+                    <el-button type="primary">添加新类型</el-button>
+                </el-col>
+            </el-row>
+            <!--表格展示区-->
             <el-table
                 class="series-table"
                 :data="tableData.showData"
@@ -76,7 +88,7 @@
                     width="120"
                 >
                     <template slot-scope="scoped">
-                        <span>{{ scoped.row.isOnline === 0 ? '未上架' : '已上架'}}</span>
+                        <span>{{ scoped.row.isOnline === 0 ? '未上架' : '已上架' }}</span>
                     </template>
                 </el-table-column>
                 <el-table-column
@@ -86,7 +98,9 @@
                 >
                     <template v-slot="scoped">
                         <el-button size="small" @click="checkRow(scoped.row)" type="primary">查看</el-button>
-                        <el-button v-if="scoped.row.isOnline === 0" size="small" @click="editRow(scoped.row)" type="success">上架</el-button>
+                        <el-button v-if="scoped.row.isOnline === 0" size="small" @click="editRow(scoped.row)"
+                                   type="success">上架
+                        </el-button>
                         <el-button v-else size="small" @click="editRow(scoped.row)" type="warning">下架</el-button>
                         <el-button size="small" @click="deleteRow(scoped.row)" type="danger">删除</el-button>
                     </template>
@@ -113,12 +127,13 @@ export default {
     name: "Window",
     data() {
         return {
+            searchInput: '',
             tableData: {
                 showData: [],
                 originData: [],
                 total: 0
             },
-            previewList:[],
+            previewList: [],
             paginationOptions: {
                 currentPage: 1,
                 pageSize: 10
@@ -157,12 +172,32 @@ export default {
                 this.$message.error('操作失败');
             }
         },
+
+        searchWindows(){
+            const key = this.searchInput;
+            const list = this.tableData.originData;
+            let newArr = [];
+
+            list.forEach(item => {
+                if (item.id.toString().indexOf(key) >= 0 || item.typeName.toString().indexOf(key) >= 0 || item.name.toString().indexOf(key) >= 0){
+                    newArr.push(item);
+                }
+            })
+            console.log(newArr);
+            this.tableData.showData = newArr;
+        },
+
+        clearSearch(){
+            this.searchInput = '';
+            this.tableData.showData = this.tableData.originData;
+        },
         /**
          * 分页组件更新页面的时候触发的回调函数
          * 用于触发获取数据的函数进行数据更新
          * @param num
          */
         pageNumChange(num) {
+            this.searchInput = '';
             this.getWindowByPageNum(num);
         }
     },
