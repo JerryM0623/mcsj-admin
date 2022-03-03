b6c5b508c504df795f0e7b36665b5150467c301a
熊熊熊子路 <1711279251@qq.com>
2022-03-03 13:39:59 +0800

    feat: 新增一个api接口

--- src/apis/goods.types.api.js
+++ src/apis/goods.types.api.js
@@ -2,7 +2,8 @@ const typesApis = {
     getTypesByPageNum: '/admin/goods/types/getByPage',
     addType: '/admin/goods/types/addType',
     editType: '/admin/goods/types/editType',
-    deleteType: '/admin/goods/types/deleteType'
+    deleteType: '/admin/goods/types/deleteType',
+    getTypesBySeries: '/admin/goods/types/getBySeries'
 }
 
 export default typesApis;
