595ad962fc54b4ab5e0a111ba918b58d3117f2f1
熊熊熊子路 <1711279251@qq.com>
2022-05-13 12:27:47 +0800

    feat: 编写全部接口

--- src/apis/order.api.js
+++ src/apis/order.api.js
@@ -1,9 +1,18 @@
 const orderRouter = '/admin/order'
 
 const orderApis = {
-    // 获取全部
+    // 分页获取全部
     getOrderByPage: `${ orderRouter }/page`,
-    getRefundOrder: `${ orderRouter }/refund`
+    // 分页获取退款订单
+    getRefundOrder: `${ orderRouter }/refund`,
+    // 编辑收货信息
+    editOrderReceiveInfo: `${ orderRouter }/edit`,
+    // 发货按钮
+    orderDelivery: `${ orderRouter }/delivery`,
+    // 同意退款
+    agreeRefund: `${ orderRouter }/agree`,
+    // 拒绝退款
+    rejectRefund: `${ orderRouter }/reject`
 }
 
 export default orderApis;
