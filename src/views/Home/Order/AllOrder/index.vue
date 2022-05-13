93326361ad6725fd41237d35cbcd73e7b5d4c40b
熊熊熊子路 <1711279251@qq.com>
2022-05-13 12:39:12 +0800

    feat: 实现搜索功能

--- src/views/Home/Order/AllOrder/index.vue
+++ src/views/Home/Order/AllOrder/index.vue
@@ -7,12 +7,12 @@
           <el-input v-model="searchInput" style="width: 100%" placeholder="请键入数据进行搜索"></el-input>
         </el-col>
         <el-col :span="18">
-          <el-button @click="searchRolePermission" type="primary" style="margin-left: 10px">搜索</el-button>
+          <el-button @click="searchOrder" type="primary" style="margin-left: 10px">搜索</el-button>
           <el-button @click="clearSearch" type="primary">清除搜索信息</el-button>
         </el-col>
       </el-row>
       <AllOrderTable
-        :table-data="tableData"
+        :table-data="showTableData"
       ></AllOrderTable>
     </el-card>
   </div>
@@ -28,7 +28,9 @@ export default {
   },
   data(){
     return {
+      searchInput: '',
       tableData: [],
+      showTableData: [],
       paginationOptions: {
         pageSize: 10,
         pageNum: 1,
@@ -51,13 +53,26 @@ export default {
           this.$message.error(res.msg);
         }else{
           this.total = res.data.total;
-          this.tableData = res.data.list;
+          this.showTableData = this.tableData = res.data.list;
         }
       })
       .catch((err) => {
         console.log(err);
         this.$message.error('数据获取失败,请稍后重试')
       })
+    },
+    // 搜索订单
+    searchOrder(){
+      const result = this.tableData.filter(item => {
+        if (item.productName.indexOf(this.searchInput) !== -1){
+          return item;
+        }
+      })
+      this.showTableData = result;
+    },
+    clearSearch(){
+      this.searchInput = '';
+      this.showTableData = this.tableData;
     }
   },
   mounted() {
